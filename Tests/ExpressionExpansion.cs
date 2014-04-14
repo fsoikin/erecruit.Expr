7adb144ce1a22c9ce4396ac896fffc2d11ac919c
Fyodor Soikin <fsoikin@erecruit.com>
2014-04-14 11:51:40 -0400

    Added tests.

---
+++ Tests/ExpressionExpansion.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace erecruit.Tests
+{
+	public class ExpressionExpansion
+	{
+		[Fact]
+		public void Should_expand_simple_call_with_one_argument()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB( a ) );
+			var e2 = Expr.Create( ( A a ) => new B { L = a.S.Length } );
+			Assert.Equal( e1.Expand().ToString(), e2.ToString() );
+		}
+
+		[Fact]
+		public void Should_expand_simple_call_with_two_arguments()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB( a, 1 ) );
+			var e2 = Expr.Create( ( A a ) => new B { L = a.S.Length + 1 } );
+			Assert.Equal( e1.Expand().ToString(), e2.ToString() );
+		}
+
+		[Fact]
+		public void Should_expand_simple_call_with_three_arguments()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB( a, 1, "xy" ) );
+			var e2 = Expr.Create( ( A a ) => new B { L = (a.S + "xy").Length + 1 } );
+			Assert.Equal( e1.Expand().ToString(), e2.ToString() );
+		}
+
+		[Fact]
+		public void Should_throw_when_invoked_without_expansion()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB( a ) );
+			Assert.Throws<InvalidOperationException>( () => e1.Compile()( new A { S = "a" } ) );
+		}
+
+		[Fact]
+		public void Should_not_expand_methods_not_marked_with_Substitute_attribute()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB_NoAttribute( a ) );
+			Assert.Equal( e1.Expand().ToString(), e1.ToString() );
+		}
+
+		[Fact]
+		public void Should_throw_when_target_method_does_not_call_Subst_Expr()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB_NoSink( a ) );
+			Assert.Throws<Exception>( () => e1.Expand() );
+		}
+
+		[Fact]
+		public void Should_throw_when_expression_has_fewer_args_than_the_enclosing_method()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB_FewerArgs( a ) );
+			Assert.Throws<InvalidOperationException>( () => e1.Expand() );
+		}
+
+		[Fact]
+		public void Should_throw_when_expression_has_more_args_than_the_enclosing_method()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB_MoreArgs( a ) );
+			Assert.Throws<InvalidOperationException>( () => e1.Expand() );
+		}
+
+		[Fact]
+		public void Should_throw_when_expression_has_args_of_types_different_from_those_of_the_enclosing_methods_args()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB_WrongTypeOfArgs( a ) );
+			Assert.Throws<InvalidOperationException>( () => e1.Expand() );
+		}
+
+		[Fact]
+		public void Should_throw_when_expression_return_type_is_different_from_the_enclosing_method()
+		{
+			var e1 = Expr.Create( ( A a ) => ToB_WrongReturnType( a ) );
+			Assert.Throws<InvalidOperationException>( () => e1.Expand() );
+		}
+
+		class A
+		{
+			public string S { get; set; }
+		}
+
+		class B
+		{
+			public int L { get; set; }
+		}
+
+		[Substitute]
+		static B ToB( A a_ ) { return Subst.Expr( ( A a ) => new B { L = a.S.Length } ); }
+
+		[Substitute]
+		static B ToB( A a_, int n_ ) { return Subst.Expr( ( A a, int n ) => new B { L = a.S.Length + n } ); }
+
+		[Substitute]
+		static B ToB( A a_, int n_, string s_ ) { return Subst.Expr( ( A a, int n, string s ) => new B { L = (a.S + s).Length + n } ); }
+
+		static B ToB_NoAttribute( A a ) { return new B { L = a.S.Length }; }
+		[Substitute] static B ToB_NoSink( A a ) { return new B { L = a.S.Length }; }
+		[Substitute] static B ToB_MoreArgs( A a ) { return Subst.Expr( ( A aa, int n ) => new B() ); }
+		[Substitute] static B ToB_FewerArgs( A a ) { return Subst.Expr( () => new B() ); }
+		[Substitute] static B ToB_WrongTypeOfArgs( A a ) { return Subst.Expr( ( B b ) => new B() ); }
+		[Substitute] static B ToB_WrongReturnType( A a ) { Subst.Expr( ( A b ) => new A() ); return null; }
+	}
+}
---
+++ Tests/ExpressionNesting.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace erecruit.Tests
+{
+	public class ExpressionNesting
+	{
+		[Fact]
+		public void Should_expand_simple_call()
+		{
+			var e = Expr.Create( ( int a ) => a + 1 );
+			var x = Expr.Create( ( string s ) => e.Call( s.Length ) );
+			var c = x.Expand();
+			Assert.Equal( 3, c.Compile()( "ab" ) );
+		}
+
+		[Fact]
+		public void Unexpanded_expression_should_throw_exception_if_compiled()
+		{
+			var e = Expr.Create( ( int a ) => a + 1 );
+			var x = Expr.Create( ( string s ) => e.Call( s.Length ) );
+			Assert.Throws<NotSupportedException>( () => x.Compile()( "ab" ) );
+		}
+
+		[Fact]
+		public void Should_expand_nested_call()
+		{
+			var e = Expr.Create( ( string a ) => a.Length );
+			var x = Expr.Create( ( int[] ii, string s ) => ii.Contains( e.Call( s ) ) );
+			var c = x.Expand();
+			Assert.Equal( true, c.Compile()( new[] { 1, 2 }, "ab" ) );
+			Assert.Equal( false, c.Compile()( new[] { 1, 3, 4 }, "ab" ) );
+		}
+
+		[Fact]
+		public void Should_expand_doubly_nested_call()
+		{
+			var e1 = Expr.Create( ( string a ) => a.Length );
+			var e2 = Expr.Create( ( string a ) => a + "1" );
+			var x = Expr.Create( ( int[] ii, string s ) => ii.Contains( e1.Call( e2.Call( s ) ) ) );
+			var c = x.Expand();
+			Assert.Equal( true, c.Compile()( new[] { 1, 3 }, "ab" ) );
+			Assert.Equal( false, c.Compile()( new[] { 1, 5, 4 }, "ab" ) );
+		}
+
+		[Fact]
+		public void Should_expand_call_nested_inside_inner_quotation()
+		{
+			var e = Expr.Create( ( string a ) => a.Length );
+			var x = Expr.Create( ( string[] ii ) => ii.Any( s => e.Call( s ) == 5 ) );
+			var c = x.Expand();
+			Assert.Equal( false, c.Compile()( new[] { "a", "ab" } ) );
+			Assert.Equal( true, c.Compile()( new[] { "a", "ab", "12345" } ) );
+		}
+
+		[Fact]
+		public void Should_expand_call_of_expression_that_itself_contains_a_call()
+		{
+			var e1 = Expr.Create( ( string a ) => a.Length );
+			var e2 = Expr.Create( ( string a ) => e1.Call( a ) == 5 );
+			var x = Expr.Create( ( string[] ii ) => ii.Any( s => e2.Call( s ) ) );
+			var c = x.Expand();
+			Assert.Equal( false, c.Compile()( new[] { "a", "ab" } ) );
+			Assert.Equal( true, c.Compile()( new[] { "a", "ab", "12345" } ) );
+		}
+
+		[Fact]
+		public void Should_expand_call_nested_inside_inner_quotation_that_does_not_contain_anything_else_besides_that_call()
+		{
+			var e = Expr.Create( ( string a ) => a.Length == 5 );
+			var x = Expr.Create( ( string[] ii ) => ii.Any( s => e.Call( s ) ) );
+			var c = x.Expand();
+			Assert.Equal( false, c.Compile()( new[] { "a", "ab" } ) );
+			Assert.Equal( true, c.Compile()( new[] { "a", "ab", "12345" } ) );
+		}
+	}
+}

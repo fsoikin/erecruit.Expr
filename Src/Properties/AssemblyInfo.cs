eaf7090ffc4d89f2a120e052dd0e10a1922f3495
Fyodor Soikin <fsoikin@erecruit.com>
2014-04-16 16:40:58 -0400

    Created separate project for .NET 4.5 - apparently Portable Class Libraries don't work in all scenarios.

--- Src/Properties/AssemblyInfo.cs
+++ Src/Properties/AssemblyInfo.cs
@@ -4,9 +4,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("erecruit.Expr")]
+[assembly: AssemblyDescription( "erecruit Expression Transform Library" )]
 [assembly: AssemblyCompany("erecruit")]
 [assembly: AssemblyProduct("erecruit.Expr")]
 [assembly: AssemblyCopyright("Copyright © erecruit 2014")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
